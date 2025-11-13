1d9a3919af78ae280c07ef9f6b79042d001cb2eb
Ariful Islam <arifulq234@gmail.com>
2025-11-13 23:24:11 +0600

    Optimized loading issue on home page latest Crops

--- src/Components/LatestProducts/LatestCrops.jsx
+++ src/Components/LatestProducts/LatestCrops.jsx
@@ -4,10 +4,12 @@ import CropCard from "../CropCard/CropCard";
 import { Link } from "react-router";
 
 const LatestProducts = () => {
+  const [fetchLoading, setFetchLoading] = useState(true);
   const Instance = useSecureInstance();
   const [lasetsCrops, SetLatestCrops] = useState([]);
   useEffect(() => {
     Instance.get("/lastest-Crops").then((data) => {
+      setFetchLoading(false);
       SetLatestCrops(data.data);
     });
   });
@@ -21,14 +23,28 @@ const LatestProducts = () => {
           Crops
         </span>
       </h1>
+      {fetchLoading ? (
+        <div
+          className="flex justify-center"
+        >
+          <span className="loading my-4  text-3xl  loading-spinner loading-xl"></span>
+        </div>
+      ) : (
+        ""
+      )}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {lasetsCrops.map((singleCrop) => (
           <CropCard key={singleCrop._id} crop={singleCrop}></CropCard>
         ))}
       </div>
-  <div className="flex justify-center">
-        <Link to={'/allcrops'} className="btn bg-gradient-to-r from-[#166534] via-[#22C55E] to-[#A3E635] text-white  px-4 py-2 w-fit">View All</Link>
-  </div>
+      <div className="flex justify-center">
+        <Link
+          to={"/allcrops"}
+          className="btn bg-gradient-to-r from-[#166534] via-[#22C55E] to-[#A3E635] text-white  px-4 py-2 w-fit"
+        >
+          View All
+        </Link>
+      </div>
     </div>
   );
 };
--- src/Pages/LoadingPage.jsx
+++ src/Pages/LoadingPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoadingPage = () => {
     return (
-        <div className='min-h-screen flex items-center justify-center'>
+        <div className='min-h-screen flex items-center space-y-2 flex-col justify-center'>
             <span className="loading loading-spinner loading-xl"></span>
             <p className='text-gray-600 text-2xl'>Loading</p>
         </div>
