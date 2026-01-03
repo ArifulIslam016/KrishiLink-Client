d2bce88509d204b97ff7bc42707e0f6d7631e341
Ariful Islam <arifulq234@gmail.com>
2026-01-04 01:13:55 +0600

    latest product page optimized

--- src/Components/LatestProducts/LatestCrops.jsx
+++ src/Components/LatestProducts/LatestCrops.jsx
@@ -32,7 +32,7 @@ const LatestProducts = () => {
       ) : (
         ""
       )}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         {lasetsCrops.map((singleCrop) => (
           <CropCard key={singleCrop._id} crop={singleCrop}></CropCard>
         ))}
